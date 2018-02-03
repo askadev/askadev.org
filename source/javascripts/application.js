067d192cdcb63ecce9d3e60d6b4ced8e5290027f
Tim Shedor <tshedor@gmail.com>
2018-02-03 12:43:18 -0800

    don't show all skills, use an object for events

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -5,5 +5,15 @@
     year.innerHTML = new Date().getFullYear();
   }
 
+  function handleShowMore(e) {
+    this.classList.remove('show-more');
+    this.style.display = 'none';
+  }
+
+  var more = document.querySelectorAll('.js-show-more');
+  more.forEach(function(item) {
+    item.addEventListener('click', handleShowMore);
+  });
+
   window.addEventListener('DOMContentLoaded', setYear);
 })();
