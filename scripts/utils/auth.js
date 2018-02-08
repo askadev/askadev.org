7fa811292ab388fe28bb54c3304cc5c952ff5d25
Will Chertoff <willchertoff@gmail.com>
2018-02-08 10:20:55 -0800

    adds githubID to UserModel

--- scripts/utils/auth.js
+++ scripts/utils/auth.js
@@ -1,4 +1,4 @@
-import { firebaseAuth, refRoot, table } from "../constants/firebase"
+import { firebaseAuth, refRoot, table } from '../constants/firebase'
 
 export function login() {
   const provider = new firebaseAuth.GithubAuthProvider()
@@ -7,15 +7,20 @@ export function login() {
     .signInWithPopup(provider)
     .then(function(result) {
       const token = result.credential.accessToken
-      const { displayName, photoURL, uid = false } = result.user
+      const { displayName, photoURL, uid = false, providerData } = result.user
 
-      // save username, github id
+      // grab githubID from providerData
+      const githubData = providerData[0] || {}
+      const { uid: githubID } = githubData
+
+      // save displayName, photoURL, and github id
       if (uid) {
         refRoot(table.USERS.BASE)
           .child(uid)
           .set({
             displayName,
-            photoURL
+            photoURL,
+            githubID
           })
       } else {
         // Log failed signup info here
