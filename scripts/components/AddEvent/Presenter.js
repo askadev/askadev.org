49fe4fec7250dcb7880bfa903b6e3b504f7f88f4
Will Chertoff <willchertoff@gmail.com>
2018-02-08 16:39:07 -0800

    adds form styling for admin pages

--- scripts/components/AddEvent/Presenter.js
+++ scripts/components/AddEvent/Presenter.js
@@ -24,53 +24,58 @@ export default class extends React.Component {
   }
   render() {
     return (
-      <div>
-        <h4>Create a new Event!</h4>
-        <form onSubmit={this.submit}>
-          <fieldset>
-            <label htmlFor="name">Event Name</label>
-            <input
-              name="name"
-              type="text"
-              placeholder="Event Name"
-              value={this.state.name}
-              onChange={e => this.setState({ name: e.target.value })}
+      <div className="layout-form">
+        <div className="inner">
+          <h2>Create a new Event!</h2>
+          <form onSubmit={this.submit}>
+            <fieldset>
+              <label htmlFor="name">Event Name</label>
+              <input
+                name="name"
+                type="text"
+                placeholder="Event Name"
+                value={this.state.name}
+                onChange={e => this.setState({ name: e.target.value })}
+              />
+            </fieldset>
+            <DateInput
+              label="Event Date"
+              onChange={date => this.setState({ date })}
             />
-          </fieldset>
-          <DateInput
-            label="Event Date"
-            onChange={date => this.setState({ date })}
-          />
-          <fieldset>
-            <label htmlFor="startTime">Start Time</label>
-            <input
-              name="startTime"
-              type="time"
-              placeholder="Event Start Time"
-              value={this.state.startTime}
-              onChange={e => this.setState({ startTime: e.target.event })}
+            <fieldset>
+              <label htmlFor="startTime">Start Time</label>
+              <input
+                name="startTime"
+                type="time"
+                placeholder="Event Start Time"
+                value={this.state.startTime}
+                onChange={e => this.setState({ startTime: e.target.event })}
+              />
+            </fieldset>
+            <fieldset>
+              <label htmlFor="endTime">End Time</label>
+              <input
+                name="endTime"
+                type="time"
+                placeholder="Event End Time"
+                value={this.state.endTime}
+                onChange={e => this.setState({ endTime: e.target.event })}
+              />
+            </fieldset>
+            <fieldset>
+              <label>Region</label>
+              <RegionSelect onChange={region => this.setState({ region })} />
+            </fieldset>
+            <Places
+              label="Location"
+              value={this.state.location}
+              onChange={location => this.setState({ location })}
             />
-          </fieldset>
-          <fieldset>
-            <label htmlFor="endTime">End Time</label>
-            <input
-              name="endTime"
-              type="time"
-              placeholder="Event End Time"
-              value={this.state.endTime}
-              onChange={e => this.setState({ endTime: e.target.event })}
-            />
-          </fieldset>
-          <fieldset>
-            <label>Region</label>
-            <RegionSelect onChange={region => this.setState({ region })} />
-          </fieldset>
-          <Places
-            label="Location"
-            value={this.state.location}
-            onChange={location => this.setState({ location })}
-          />
-        </form>
+            <fieldset>
+              <button type="submit">Add Event</button>
+            </fieldset>
+          </form>
+        </div>
       </div>
     )
   }
