0c17d888ea7295517617372c163800c63eecaf15
Will Chertoff <willchertoff@gmail.com>
2018-02-09 11:57:11 -0800

    fixes location name text field

--- scripts/components/AddEvent/Presenter.js
+++ scripts/components/AddEvent/Presenter.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
-import { DateInput, TextInput, TimeInput, PlacesInput, SelectInput } from '../Inputs'
+import {
+  DateInput,
+  TextInput,
+  TimeInput,
+  PlacesInput,
+  SelectInput
+} from '../Inputs'
 import RegionSelect from '../RegionSelect'
 
 import FormLayout from '../Layouts/Form'
@@ -8,7 +14,12 @@ import FormLayout from '../Layouts/Form'
 import { createEvent } from '../../actions/events'
 
 function appendTime(times) {
-  return this.date.clone().hours(times[0]).minutes(times[1]).utc().valueOf()
+  return this.date
+    .clone()
+    .hours(times[0])
+    .minutes(times[1])
+    .utc()
+    .valueOf()
 }
 
 function regionOptions(regions) {
@@ -34,7 +45,13 @@ export default class extends React.Component {
 
   handleSubmit = e => {
     e.preventDefault()
-    if (!this.date || !this.startTime || !this.endTime || !this.state.displayName || !this.state.mapsUrl) {
+    if (
+      !this.date ||
+      !this.startTime ||
+      !this.endTime ||
+      !this.state.displayName ||
+      !this.state.mapsUrl
+    ) {
       return alert('All fields are required')
     }
     const startTime = appendTime.call(this, this.startTime)
@@ -48,22 +65,24 @@ export default class extends React.Component {
       mapsUrl: this.state.mapsUrl,
       locationData: this.locationData
     })
-    .then(res => alert('Event successfully created'))
-    .catch(res => alert('Error creating event'))
+      .then(res => alert('Event successfully created'))
+      .catch(res => alert('Error creating event'))
   }
 
   render() {
     return (
-      <FormLayout title={`Create a new Event in ${this.props.regionDisplayName}`}>
+      <FormLayout
+        title={`Create a new Event in ${this.props.regionDisplayName}`}
+      >
         <form onSubmit={this.handleSubmit}>
-          {this.props.isSuperAdmin &&
+          {this.props.isSuperAdmin && (
             <SelectInput
               label="Region"
               defaultValue={this.props.userCurrentRegion}
               options={regionOptions(this.props.regions)}
-              inputRef={val => this.region = val}
+              inputRef={val => (this.region = val)}
             />
-          }
+          )}
 
           <PlacesInput
             label="Location"
@@ -74,7 +93,7 @@ export default class extends React.Component {
           <TextInput
             label="Location Name"
             placeholder="Ye Old Coffee"
-            onChange={displayName => this.setState({ displayName })}
+            onChange={e => this.setState({ displayName: e.target.value })}
             value={this.state.displayName}
           />
 
@@ -85,16 +104,12 @@ export default class extends React.Component {
             value={this.state.mapsUrl}
           />
 
-          <DateInput
-            label="Event Date"
-            onChange={val => this.date = val}
-          />
+          <DateInput label="Event Date" onChange={val => (this.date = val)} />
           <TimeInput
             label="Start Time"
-            onChange={val => this.startTime = val} />
-          <TimeInput
-            label="End Time"
-            onChange={val => this.endTime = val} />
+            onChange={val => (this.startTime = val)}
+          />
+          <TimeInput label="End Time" onChange={val => (this.endTime = val)} />
           <input type="submit" value="Add Event" />
         </form>
       </FormLayout>
